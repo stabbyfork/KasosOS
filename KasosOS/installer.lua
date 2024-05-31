64a7f5d3edd0d1a192e9e77bad33bf1770987fe9
stabbyfork <138290921+stabbyfork@users.noreply.github.com>
2024-05-31 18:27:09 +0200

    Add files via upload

--- KasosOS/installer.lua
+++ KasosOS/installer.lua
@@ -1,1 +1,16 @@
-
+if not fs.exists("PC") then
+    fs.makeDir("PC")
+else
+    print("PC directory already exists")
+end
+fs.makeDir("PC/desktop")
+fs.makeDir("PC/system")
+fs.makeDir("PC/system/assets")
+fs.makeDir("PC/system/config")
+fs.makeDir("PC/system/logs")
+fs.makeDir("PC/system/users")
+fs.makeDir("PC/system/bootloader")
+fs.makeDir("PC/system/assets/desktop")
+fs.makeDir("PC/system/assets/system")
+fs.makeDir("PC/system/lib")
+print("Installer complete")
