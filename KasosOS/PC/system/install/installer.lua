ac9718e1014c5b35a1aca1d1a66b00d60d7481e8
stabbyfork <138290921+stabbyfork@users.noreply.github.com>
2024-05-31 18:44:37 +0200

    Add files via upload

--- KasosOS/PC/system/install/installer.lua
+++ KasosOS/PC/system/install/installer.lua
@@ -3,6 +3,7 @@ if not fs.exists("PC") then
 else
     print("PC directory already exists")
 end
+
 fs.makeDir("PC/desktop")
 fs.makeDir("PC/system")
 fs.makeDir("PC/system/assets")
@@ -13,4 +14,14 @@ fs.makeDir("PC/system/bootloader")
 fs.makeDir("PC/system/assets/desktop")
 fs.makeDir("PC/system/assets/system")
 fs.makeDir("PC/system/lib")
+fs.makeDir("PC/system/misc")
+fs.makeDir("PC/system/install")
+
+local installList = "https://github.com/stabbyfork/KasosOS/blob/main/KasosOS/PC/system/install/install.txt"
+local toInstall = http.get(installList).readAll()
+
+for line in toInstall:gmatch("[^\n]+") do
+    shell.run("wget", "run", line)
+end
+
 print("Installer complete")
