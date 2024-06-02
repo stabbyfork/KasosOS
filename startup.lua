195d3368e5ac15962db52a3c64a9b72a69e68b46
stabbyfork <markus2018mmh@gmail.com>
2024-06-02 11:11:58 +0300

    adding lockscreen

--- startup.lua
+++ startup.lua
@@ -1,8 +1,24 @@
 local basalt = require("/KasosOS/PC/system/lib/basalt")
+local screen = term.current()
 
 local mainFrame = basalt.createFrame()
---local image = mainFrame:addImage()
 mainFrame:addTexture("/KasosOS/PC/system/assets/desktop/pack.bimg")
 mainFrame:setTextureMode("center")
 
+local time = os.time()
+local function updateTime()
+    while true do
+        time = os.time()
+        print(textutils.formatTime(time))
+        os.sleep(1)
+    end
+end
+
+local function waitForInput()
+    os.pullEvent()
+    print("Something was done")
+end
+
+parallel.waitForAny(updateTime, waitForInput)
+print("complete")
 basalt.autoUpdate()
