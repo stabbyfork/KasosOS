3fe74cd2aef128594d3b368be4d9985e03f151dd
stabbyfork <markus2018mmh@gmail.com>
2024-06-02 19:45:01 +0300

    fixed errors

--- KasosOS/PC/system/install/installer.lua
+++ KasosOS/PC/system/install/installer.lua
@@ -37,7 +37,9 @@ for line in string.gmatch(toInstall.readAll(), "[^\r\n]+") do
     elseif firstChar == "?" then -- github repo
         local request, err, errResp = http.get({url=line:sub(2), headers={["Accept"]="application/vnd.github.raw+json"}})
         if err then
-            print(err, errResp)
+            print(err, textutils.serialise(errResp))
+            request.close()
+            goto continue
         end
         downloadRepoRecursive(request)
         request.close()
