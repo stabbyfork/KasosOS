43c55cd849baa46bdf98052c21e2d13a91fad718
stabbyfork <markus2018mmh@gmail.com>
2024-06-02 09:40:13 +0300

    finally starting UI

---
+++ startup.lua
@@ -0,0 +1,5 @@
+local basalt = require("/KasosOS/PC/system/lib/basalt")
+local mainFrame = basalt.createFrame()
+mainFrame:addTexture("/KasosOS/PC/system/assets/desktop/pack.nfp")
+
+basalt.autoUpdate()
