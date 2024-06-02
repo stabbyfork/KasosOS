097164d837dac07036ac757d46c6bf022aaa1ed0
stabbyfork <markus2018mmh@gmail.com>
2024-06-02 11:22:08 +0300

    added to startup

--- startup.lua
+++ startup.lua
@@ -1,9 +1,9 @@
 local basalt = require("/KasosOS/PC/system/lib/basalt")
 local screen = term.current()
 
-local mainFrame = basalt.createFrame()
-mainFrame:addTexture("/KasosOS/PC/system/assets/desktop/pack.bimg")
-mainFrame:setTextureMode("center")
+--local mainFrame = basalt.createFrame()
+--mainFrame:addTexture("/KasosOS/PC/system/assets/desktop/pack.bimg")
+--mainFrame:setTextureMode("center")
 
 local time = os.time()
 local function updateTime()
@@ -15,8 +15,17 @@ local function updateTime()
 end
 
 local function waitForInput()
-    os.pullEvent()
-    print("Something was done")
+    while true do
+        local event, param = os.pullEvent()
+        if event == "key" or "mouse_click" then
+            return
+        elseif event == "monitor_touch" then
+            screen = peripheral.wrap(param)
+            term.redirect(screen)
+            print("monitor touched")
+            return
+        end
+    end
 end
 
 parallel.waitForAny(updateTime, waitForInput)
