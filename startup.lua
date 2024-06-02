bfbb8edc0de622ff5861b8aae7f09ab808d9ab4f
stabbyfork <markus2018mmh@gmail.com>
2024-06-02 10:15:09 +0300

    testing 2

--- startup.lua
+++ startup.lua
@@ -1,8 +1,6 @@
 local basalt = require("/KasosOS/PC/system/lib/basalt")
 local mainFrame = basalt.createFrame()
---[[mainFrame
-    :setPosition(1,1)
-    :setSize(30,30)
-    :addTexture("/KasosOS/PC/system/assets/desktop/pack.nfp")--]]
+local image = mainFrame:addImage()
+image:loadImage("/KasosOS/PC/system/assets/desktop/pack.nfp")
 
 basalt.autoUpdate()
