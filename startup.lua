1c923a5874e37b60984c2549f2e2fabb5569a752
stabbyfork <markus2018mmh@gmail.com>
2024-06-02 10:49:20 +0300

    fixing desktop

--- startup.lua
+++ startup.lua
@@ -1,7 +1,7 @@
 local basalt = require("/KasosOS/PC/system/lib/basalt")
 
 local mainFrame = basalt.createFrame()
-local image = mainFrame:addImage()
-image:loadImage("/KasosOS/PC/system/assets/desktop/pack.bimg")
+--local image = mainFrame:addImage()
+mainFrame:addTexture("/KasosOS/PC/system/assets/desktop/pack.bimg")
 
 basalt.autoUpdate()
